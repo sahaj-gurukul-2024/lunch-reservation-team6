202763de12b054e2df1de389dc30766fcde72f4d
Aravind265 <aravindkumarv2657@gmail.com>
2024-02-22 16:02:31 +0530

    Aravind | first setup

---
+++ src/test/kotlin/gkl/exercise/LunchReservationServerTest.kt
@@ -0,0 +1,20 @@
+package gkl.exercise
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+@MicronautTest
+class LunchReservationServerTest(@Client("/") val client: HttpClient) {
+
+    @Test
+    fun testHello() {
+        val request: HttpRequest<Any> = HttpRequest.GET("/hello")
+        val body = client.toBlocking().retrieve(request)
+        assertNotNull(body)
+        assertEquals("Hello World", body)
+    }
+
+}
