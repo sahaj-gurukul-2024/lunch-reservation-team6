efd4b6bbc0984a2d032f3535ae73ff337839ef75
Madhvesh77 <madhvesh77@gmail.com>
2024-02-24 17:30:27 +0530

    Madhvesh | Written status code check test for reservtion api.

--- src/test/kotlin/gkl/exercise/LunchReservationServerTest.kt
+++ src/test/kotlin/gkl/exercise/LunchReservationServerTest.kt
@@ -5,6 +5,8 @@ import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import gkl.exercise.models.Employee
+import gkl.exercise.models.PrimaryComposite
+import gkl.exercise.models.Reservation
 import gkl.exercise.repository.EmployeeRepository
 import gkl.exercise.services.LoginServices
 import io.micronaut.http.HttpStatus
@@ -19,6 +21,9 @@ class LunchReservationServerTest(@Client("/") val client: HttpClient, val object
 
     @Inject
     lateinit var loginController: LoginController
+
+    @Inject
+    lateinit var reservationController: ReservationController
 
     @Inject
     lateinit var loginServices: LoginServices
@@ -55,6 +60,15 @@ class LunchReservationServerTest(@Client("/") val client: HttpClient, val object
         var httpResponse = loginController.validateUser(employee1)
         httpResponse = loginController.validateUser(employee2)
         assertEquals("Madhvesh", employeeRepository.findById(10).get().name)
+    }
+
+    @Test
+    fun `Should be able to reserve lunch for any day and existing employee`() {
+        val employee = Employee(id = 101, name = "Madhvesh")
+        var httpResponse = loginController.validateUser(employee)
+        val reservation = Reservation(PrimaryComposite(101, "22-02-2024"),"Madhvesh", true)
+        httpResponse = reservationController.reserveUser(reservation)
+        assertEquals(200, httpResponse.status.code)
     }
 
 }
