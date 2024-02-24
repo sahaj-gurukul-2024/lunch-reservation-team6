57bbcb3684fc3f470ed5b959702d4f54f48b52af
Madhvesh77 <madhvesh77@gmail.com>
2024-02-24 17:20:34 +0530

    Written all possible test cases for login api.

--- src/test/kotlin/gkl/exercise/LunchReservationServerTest.kt
+++ src/test/kotlin/gkl/exercise/LunchReservationServerTest.kt
@@ -5,6 +5,7 @@ import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import gkl.exercise.models.Employee
+import gkl.exercise.repository.EmployeeRepository
 import gkl.exercise.services.LoginServices
 import io.micronaut.http.HttpStatus
 import io.micronaut.serde.ObjectMapper
@@ -22,6 +23,9 @@ class LunchReservationServerTest(@Client("/") val client: HttpClient, val object
     @Inject
     lateinit var loginServices: LoginServices
 
+    @Inject
+    lateinit var employeeRepository: EmployeeRepository
+
     @Test
     fun testHello() {
         val request: HttpRequest<Any> = HttpRequest.GET("/hello")
@@ -35,6 +39,22 @@ class LunchReservationServerTest(@Client("/") val client: HttpClient, val object
         val employee = Employee(id = 1, name = "Vaibhav")
         val httpResponse = loginController.validateUser(employee)
         assertEquals(200, httpResponse.status.code)
+    }
+
+    @Test
+    fun `Should be able to login with same id but different name`() {
+        val employee = Employee(id= 1, name = "Selva")
+        val httpResponse = loginController.validateUser(employee)
+        assertEquals(200, httpResponse.status.code)
+    }
+
+    @Test
+    fun `Override name of the employee when logging in with same id but different name`() {
+        val employee1 = Employee(id=10, name="ragul")
+        val employee2 = Employee(id=10, name = "Madhvesh")
+        var httpResponse = loginController.validateUser(employee1)
+        httpResponse = loginController.validateUser(employee2)
+        assertEquals("Madhvesh", employeeRepository.findById(10).get().name)
     }
 
 }
