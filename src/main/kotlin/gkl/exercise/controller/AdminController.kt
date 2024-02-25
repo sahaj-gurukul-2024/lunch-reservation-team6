002c05ed4f9e941badd0eb66fcec1837444ae3b2
Madhvesh77 <madhvesh77@gmail.com>
2024-02-25 18:15:16 +0530

    Madhvesh | Implement admin week endpoint.

--- src/main/kotlin/gkl/exercise/controller/AdminController.kt
+++ src/main/kotlin/gkl/exercise/controller/AdminController.kt
@@ -1,15 +1,19 @@
 package gkl.exercise.controller
 
+import gkl.exercise.models.Dates
 import gkl.exercise.services.AdminServices
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.HttpResponse
 import java.util.Date
 
 @Controller("/admin")
 class AdminController(private val adminService: AdminServices){
     @Post("/week")
-    fun showWeek(startDate: Date) {
-
+    fun showWeek(@Body Dates: Dates): MutableMap<Date, Long> {
+        val dateToCount = adminService.displayWeek(Dates)
+        return dateToCount
     }
 
 }
--- src/main/kotlin/gkl/exercise/repository/ReservationRepository.kt
+++ src/main/kotlin/gkl/exercise/repository/ReservationRepository.kt
@@ -3,9 +3,11 @@ package gkl.exercise.repository
 import gkl.exercise.entities.ReservationEntity
 import io.micronaut.data.annotation.Repository
 import io.micronaut.data.repository.CrudRepository
+import java.util.Date
 
 @Repository
 interface ReservationRepository : CrudRepository<ReservationEntity,Long>{
 
     fun findByEmpid(id : Long) : ReservationEntity?
+    fun countByDate(date: Date): Long
 }
